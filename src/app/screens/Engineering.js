ecfa78c7ad86f72412a04b37ab35db23c0a24db3
Keith-Ocitti <83019128+Keith-Ocitti@users.noreply.github.com>
2023-11-09 15:36:31 +0300

    Add files via upload

--- src/app/screens/Engineering.js
+++ src/app/screens/Engineering.js
@@ -12,175 +12,349 @@ const Engineering = () => {
       <br />
       <div className="body-container">
         <CenterHeader heading={"Engineering and Construction Services"} />
-        <div className="content-holder">
-          <Image
-            src={"/arch.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            priority
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Architectural consultancy</h4>
-            <p>
-              We organize spaces, design functional and aesthetically pleasing
-              architectural master pieces. Our expertise stretches from
-              residential houses to commercial houses and also institutional
-              buildings like schools and churches. We also consider
-              sustainability at the background of our architectural designs.
-            </p>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseOne"
+                aria-expanded="true"
+                aria-controls="panelsStayOpen-collapseOne"
+              >
+                Architectural consultancy
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="panelsStayOpen-headingOne"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/arch.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    priority
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Architectural consultancy</h4>
+                    <p>
+                      We organize spaces, design functional and aesthetically
+                      pleasing architectural master pieces. Our expertise
+                      stretches from residential houses to commercial houses and
+                      also institutional buildings like schools and churches. We
+                      also consider sustainability at the background of our
+                      architectural designs.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/land.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Landscape Designing</h4>
-            <p>
-              We design compounds around already designed architectural master
-              pieces, we consider different hardscapes and greens capes. Our
-              professional teams look at cost efficiency in our landscape
-              designs and sustainability.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Landscape Designing
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/land.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Landscape Designing</h4>
+                    <p>
+                      We design compounds around already designed architectural
+                      master pieces, we consider different hardscapes and greens
+                      capes. Our professional teams look at cost efficiency in
+                      our landscape designs and sustainability.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/int.webp"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Interior Designing</h4>
-            <p>
-              We also design the interior spaces of different types of buildings
-              inclusive of residential homes and commercial spaces. We consider
-              all design aspects to achieve maximum comfort and aesthetic
-              beauty.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Farm Designing
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/farm.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Farm Designing</h4>
+                    <p>
+                      With our experience and exposure in farms and other
+                      agricultural spaces. We design different farm structures
+                      and greens capes within the farm.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/farm.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Farm Designing</h4>
-            <p>
-              With our experience and exposure in farms and other agricultural
-              spaces. We design different farm structures and greens capes
-              within the farm.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Master Planning
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/gend.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Master Planning</h4>
+                    <p>
+                      Here we look at Organizing spaces at a larger scale like a
+                      community or planning very huge chunks of land to provide
+                      road networks and demarcating different activities on the
+                      site.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/gend.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Master Planning</h4>
-            <p>
-              Here we look at Organizing spaces at a larger scale like a
-              community or planning very huge chunks of land to provide road
-              networks and demarcating different activities on the site.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Special Furniture Designing
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/funi.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Special Furniture Designing</h4>
+                    <p>
+                      We also look at designing customized furniture to meet the
+                      needs of different home users and offices that are
+                      interested in uniqueness.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/funi.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Special Furniture Designing</h4>
-            <p>
-              We also look at designing customized furniture to meet the needs
-              of different home users and offices that are interested in
-              uniqueness.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Aquariums and Water Fountains Designing
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/aquaft.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Aquariums and Water Fountains Designing</h4>
+                    <p>
+                      Our expertise goes to the details of designing aquariums
+                      and water fountains. We also implement them on site
+                      according to the needs of the client.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/aquaft.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Aquariums and Water Fountains Designing</h4>
-            <p>
-              Our expertise goes to the details of designing aquariums and water
-              fountains. We also implement them on site according to the needs
-              of the client.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Interior Designing
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/int.webp"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Interior Designing</h4>
+                    <p>
+                      We also design the interior spaces of different types of
+                      buildings inclusive of residential homes and commercial
+                      spaces. We consider all design aspects to achieve maximum
+                      comfort and aesthetic beauty.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/pool2.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Swimming pool Designing</h4>
-            <p>
-              We design different types of swimming pools and implement them
-              through our partnerships with experts in pool construction.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Swimming pool Designing
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/pool2.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Swimming pool Designing</h4>
+                    <p>
+                      We design different types of swimming pools and implement
+                      them through our partnerships with experts in pool
+                      construction.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/app/screens/Entertainment.js
+++ src/app/screens/Entertainment.js
@@ -11,74 +11,142 @@ const Entertainment = () => {
       <br />
       <div className="body-container">
         <CenterHeader heading={"Entertainment Services"} />
-        <div className="content-holder">
-          <Image
-            src={"/brand.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Artist Brand Management</h4>
-            <p>
-              In the dynamic world of entertainment, establishing a strong brand
-              for artists is pivotal. We do the profiling of Artists, create an
-              environment where they can easily grow and keep interaction with
-              their target audience, and keep them on the front page of current
-              events
-            </p>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+              <button
+                class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseOne"
+                aria-expanded="true"
+                aria-controls="panelsStayOpen-collapseOne"
+              >
+                Artist Brand Management
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="panelsStayOpen-headingOne"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/brand.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Artist Brand Management</h4>
+                    <p>
+                      In the dynamic world of entertainment, establishing a
+                      strong brand for artists is pivotal. We do the profiling
+                      of Artists, create an environment where they can easily
+                      grow and keep interaction with their target audience, and
+                      keep them on the front page of current events
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className="content-holder">
-          <Image
-            src={"/dist.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Music Distribution and promotion</h4>
-            <p>
-              The heartbeat of the entertainment industry is music, and our
-              company serves as a gateway for artists to reach a wider audience.
-              We offer comprehensive music distribution and promotion services
-              tailored to uplift and showcase the diverse sounds of Ugandan
-              talent. Our streamlined distribution network ensures that the
-              music reaches various digital platforms and streaming services
-              worldwide.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Music Distribution and promotion
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/dist.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Music Distribution and promotion</h4>
+                    <p>
+                      The heartbeat of the entertainment industry is music, and
+                      our company serves as a gateway for artists to reach a
+                      wider audience. We offer comprehensive music distribution
+                      and promotion services tailored to uplift and showcase the
+                      diverse sounds of Ugandan talent. Our streamlined
+                      distribution network ensures that the music reaches
+                      various digital platforms and streaming services
+                      worldwide.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="content-holder">
-          <Image
-            src={"/events.jpg"}
-            height={260}
-            width={380}
-            alt="content img"
-            style={{
-              objectFit: "fill",
-            }}
-            className="img"
-          />
-          <div className="body-text">
-            <h4>Events Management</h4>
-            <p>
-              We put together awesome events like parties and concerts. We pick
-              the right place, choose which musicians will perform, and take
-              care of all the planning. Our goal is to make sure everyone has a
-              great time and enjoys the music. We want these events to be
-              memorable and fun for both the musicians and the people who come
-              to see them.
-            </p>
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#panelsStayOpen-collapseTwo"
+                aria-expanded="false"
+                aria-controls="panelsStayOpen-collapseTwo"
+              >
+                Events Management
+              </button>
+            </h2>
+            <div
+              id="panelsStayOpen-collapseTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="panelsStayOpen-headingTwo"
+            >
+              <div class="accordion-body">
+                <div className="content-holder">
+                  <Image
+                    src={"/events.jpg"}
+                    height={260}
+                    width={380}
+                    alt="content img"
+                    style={{
+                      objectFit: "fill",
+                    }}
+                    className="img"
+                  />
+                  <div className="body-text">
+                    <h4>Events Management</h4>
+                    <p>
+                      We put together awesome events like parties and concerts.
+                      We pick the right place, choose which musicians will
+                      perform, and take care of all the planning. Our goal is to
+                      make sure everyone has a great time and enjoys the music.
+                      We want these events to be memorable and fun for both the
+                      musicians and the people who come to see them.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
